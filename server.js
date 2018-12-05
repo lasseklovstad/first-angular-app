663ab4b07c929973d368a9c0d2f705d187619075
lasseklovstad <lasse.klovstad>
2018-12-05 16:27:00 +0100

    workbox

--- server.js
+++ server.js
@@ -1,19 +1,35 @@
-
 //Install express server
 const express = require('express');
 const path = require('path');
-const enforce = require('express-sslify');
+const workboxBuild = require('workbox-build');
+
+workboxBuild.generateSW({
+  swDest: './dist/my-first-angular/sw.js',
+  globDirectory: './dist/my-first-angular',
+  globPatterns: ['**/*.{html,js,css,ico}'],
+  runtimeCaching: [{
+    urlPattern: new RegExp('https://api.entur.org/'),
+    handler: 'staleWhileRevalidate',
+    
+  }]
+}).then(() => {
+  console.log("Generated Service worker")
+}).catch(err => {
+  console.log("Failed to generate Service worker", err)
+})
+
+//const enforce = require('express-sslify');
 
 const app = express();
-app.use(enforce.HTTPS({trustProtoHeader:true}));
+//app.use(enforce.HTTPS({trustProtoHeader:true}));
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/my-first-angular'));
 
-app.get('/*', function(req,res) {
+app.get('/*', function (req, res) {
 
-  res.sendFile(path.join(__dirname+'/dist/my-first-angular/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/my-first-angular/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8081);
