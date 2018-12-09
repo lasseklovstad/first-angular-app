83c90ca8be4a900c8012908bb98b98e34f29dc46
lasseklovstad <lasse.klovstad>
2018-12-09 17:10:06 +0100

    fie

--- src/sw.js
+++ src/sw.js
@@ -12,7 +12,7 @@ if(workbox){
 
   workbox.routing.registerRoute(
     new RegExp('https://api.entur.org/'),
-    workbox.strategies.networkFirst({
+    new workbox.strategies.NetworkFirst({
       cacheName:'runtime-cache',
       plugins:[new workbox.expiration.Plugin({
         maxEntries:1,
