27ebb6d1690e27fac0b5f3787c4fd729f3b7f30c
lasseklovstad <lasse.klovstad>
2018-12-09 14:30:00 +0100

    fmds

--- src/app/rutetiderdetaljer/rutetiderdetaljer.component.ts
+++ src/app/rutetiderdetaljer/rutetiderdetaljer.component.ts
@@ -15,7 +15,7 @@ export class RutetiderdetaljerComponent implements OnInit{
 
 
   departures: Departure[];
-
+  id:string;
 
   constructor(
     private ruteTiderService: RutetiderService,
@@ -24,38 +24,85 @@ export class RutetiderdetaljerComponent implements OnInit{
     private location: Location) {
   }
 
+
+
   ngOnInit() {
+
+    this.id = this.route.snapshot.paramMap.get('id');
+    this.fetchData();
+
+  }
+
+  refresh(){
+    this.fetchData();
+  }
+
+  fetchData(){
     this.spinner.show();
-    let id = this.route.snapshot.paramMap.get('id');
-    console.log(id);
-    this.ruteTiderService.getRutetider(id).subscribe((rutetider) => {
+    this.ruteTiderService.getRutetider(this.id).subscribe((rutetider) => {
 
       this.departures = rutetider['data']['stopPlace']['estimatedCalls'];
-      let now = new Date();
-      let min: number;
-      let hour: number;
-      // calculate actual arrival time;
-      for (let departure of this.departures) {
-        let idNumber = departure.serviceJourney.journeyPattern.line.id.match(/\d+/g);
-        departure.bussNumber=idNumber[0];
 
-        departure.expectedArrivalTime = parse(departure.expectedArrivalTime);
-        departure.aimedArrivalTime = parse(departure.aimedArrivalTime);
-
-        min = departure.expectedArrivalTime.getMinutes() - now.getMinutes();
-        hour = departure.expectedArrivalTime.getHours() - now.getHours();
-        if (hour == 1) {
-          departure.arrival = 60 + min + 'm';
-        }
-        else if (min == 0 && hour == 0) {
-          departure.arrival = departure.expectedArrivalTime.getSeconds() - now.getSeconds() + 's';
-        } else {
-          departure.arrival = min + 'm';
-        }
-      }
+      this.calcArrival()
+      //this.removeDuplicates()
       this.spinner.hide();
 
     });
+  }
+
+  calcArrival(){
+    let now = new Date();
+    let min: number;
+    let hour: number;
+    // calculate actual arrival time;
+    for (let departure of this.departures) {
+      let idNumber = departure.serviceJourney.journeyPattern.line.id.match(/\d+/g);
+      departure.bussNumber=idNumber[0];
+
+      departure.expectedArrivalTime = parse(departure.expectedArrivalTime);
+      departure.aimedArrivalTime = parse(departure.aimedArrivalTime);
+
+      min = departure.expectedArrivalTime.getMinutes() - now.getMinutes();
+      hour = departure.expectedArrivalTime.getHours() - now.getHours();
+      if (hour >0) {
+        departure.arrival = hour*60 + min + 'm';
+      }
+      else if (min == 0 ) {
+        departure.arrival = departure.expectedArrivalTime.getSeconds() - now.getSeconds() + 's';
+      } else {
+        departure.arrival = min + 'm';
+      }
+    }
+  }
+
+  removeDuplicates(){
+    let newArray:Departure[];
+    newArray=[];
+    while(this.departures.length>0){
+      let departure=this.departures[0];
+      let name1=this.departures[0].destinationDisplay.frontText;
+      let count=1;
+
+      for(let j = 1;j<this.departures.length;j++){
+
+        let name2=this.departures[j].destinationDisplay.frontText;
+
+        if(name1==name2){
+
+          departure.arrival+=' ,'+this.departures[j].arrival;
+          this.departures.splice(j,j+1);
+          j-=1;
+          count++;
+        }
+      }
+      this.departures.splice(0,1);
+      console.log(name1,count);
+      newArray.push(departure);
+
+    }
+
+    this.departures=newArray;
+
   }
 
   goBack(){
--- src/sw.js
+++ src/sw.js
@@ -1,11 +1,32 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js');
 
 if(workbox){
   console.log("WorkBox Works!")
   workbox.precaching.precacheAndRoute([]);
-
+  workbox.core.setLogLevel(workbox.core.LOG_LEVELS.debug);
   workbox.skipWaiting();
   workbox.clientsClaim();
+
+  const handlerCb = ({url,event,params})=>{
+    console.log("handle post request")
+    return fetch(event.request)
+      .then((response) => {
+        return response;
+      }).then((res)=>{
+        caches.open('runtime-cache').then((cache)=>{
+          console.log("open cache")
+
+          cache.put(url,res).then(()=>{
+            console.log("added to cache")
+          })
+        })
+      })
+      .catch(err=>{
+        console.log("fecth:",err)
+      })
+
+  }
+
   workbox.routing.registerRoute(
     new RegExp('https://api.entur.org/'),
     new workbox.strategies.StaleWhileRevalidate({
@@ -15,6 +36,11 @@ if(workbox){
       })]
       }
     )
+  );
+  workbox.routing.registerRoute(
+    new RegExp('https://api.entur.org/journeyplanner/'),
+    handlerCb,
+    'POST'
   );
 }else{
   console.log("Workbox isn't supported")
