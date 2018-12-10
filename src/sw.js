34a346954a836eab9ee3e71b7527eb261733b34e
lasseklovstad <lasse.klovstad>
2018-12-10 14:56:48 +0100

    remove skipwaiting

--- src/sw.js
+++ src/sw.js
@@ -2,23 +2,18 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox
 
 if(workbox){
   workbox.setConfig({debug:true});
+  workbox.skipWaiting();
+  workbox.clientsClaim();
   console.log("WorkBox Works!")
   workbox.precaching.precacheAndRoute([]);
 
- // workbox.skipWaiting();
-  workbox.clientsClaim();
+
 
 
 
   workbox.routing.registerRoute(
     new RegExp('https://api.entur.org/'),
-    new workbox.strategies.NetworkFirst({
-      cacheName:'runtime-cache',
-      plugins:[new workbox.expiration.Plugin({
-        maxEntries:1,
-      })]
-      }
-    )
+    new workbox.strategies.NetworkFirst()
   );
 
 }else{
