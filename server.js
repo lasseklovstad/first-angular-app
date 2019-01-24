618defee28d68438472c87ab0c264c188f7aafa2
lasseklovstad <lasse.klovstad>
2019-01-24 12:08:22 +0100

    fmdks

--- server.js
+++ server.js
@@ -15,6 +15,8 @@ let images = [];
 const app = express();
 app.use(bodyParser.json({limit: '5mb'}));
 app.use(bodyParser.urlencoded({limit: '5mb'}));
+
+
 //app.use(enforce.HTTPS({trustProtoHeader:true}));
 if(process.env.NODE_ENV=='production'){
   app.use(enforce.HTTPS({ trustProtoHeader: true }))
--- src/app/departure.ts
+++ src/app/departure.ts
@@ -1,8 +1,8 @@
-export class Departure{
-  expectedArrivalTime:Date;
-  serviceJourney:{journeyPattern:{line:{id:string,name:string,transportMode:string}}}
-  arrival:string;
-  aimedArrivalTime:Date;
-  destinationDisplay:{frontText:string};
-  bussNumber:string;
+export class Departure {
+  expectedArrivalTime: Date;
+  serviceJourney: { journeyPattern: { line: { id: string, name: string, transportMode: string } } };
+  arrival: string;
+  aimedArrivalTime: Date;
+  destinationDisplay: { frontText: string };
+  bussNumber: string;
 }
